78ba585eff425a844b750606a892ea9c7c5c9a0f
AaronMullan <aaroncmullan@gmail.com>
2019-10-30 15:22:53 -0700

    I think it works

--- test/render-gallery-card.test.js
+++ test/render-gallery-card.test.js
@@ -17,7 +17,7 @@ test('renders html from data', assert => {
     
     const expected = 	
     	
-"<li class="monster-card"> <h2 class="card-title">UniWhal</h2> <img class="card-image" src="http://3.bp.blogspot.com/_DBYF1AdFaHw/TE-f0cDQ24I/AAAAAAAACZg/l-FdTZ6M7z8/s1600/Unicorn_and_Narwhal_by_dinglehopper.jpg" alt="UniWhal"> <p class="horn-count">Horn count: 1</p> </li>"
+"<li class=\"monster-card\"> <h2 class=\"card-title\">UniWhal</h2> <img class=\"card-image\" src=\"http://3.bp.blogspot.com/_DBYF1AdFaHw/TE-f0cDQ24I/AAAAAAAACZg/l-FdTZ6M7z8/s1600/Unicorn_and_Narwhal_by_dinglehopper.jpg\" alt=\"UniWhal\"> <p class=\"horn-count\">Horn count: 1</p> </li>"
 
     const html = renderGalleryCard(images);
 
